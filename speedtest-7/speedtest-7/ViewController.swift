81b15b89eb9bf17197bf22998b0b6af65398fad1
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-10-07 18:51:36 +0300

    speedtest Done

--- speedtest-7/speedtest-7/ViewController.swift
+++ speedtest-7/speedtest-7/ViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var name = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let name = sender as! String
+        let vc = segue.destination as! DetailVC
+        vc.name = name 
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        name = textField.text!
+       
+        if textField.text!.count >= 2{
+             performSegue(withIdentifier: "goToNext", sender: name)
+
+        }
+        else{
+            print("لا تستعبط ")
+        }
     }
     
     // Hint: `performSegue` is the way
